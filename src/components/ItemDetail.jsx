d96845538f845ac5c4fb4f4c311d0f5291136769
Render78 <mariojuliantor@hotmail.com>
2023-12-26 20:55:45 -0300

    estilos agregados al ver detalle de un producto

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
+import { Card } from 'react-bootstrap';
 
 const ItemDetail = ({ item }) => {
     return (
-        <div className='container'>
-            <div className="producto-detalle">
-                <img src={item.imagen} alt={item.nombre} />
-                <div>
-                    <h3 className='titulo'>{item.nombre}</h3>
-                    <p className='descripcion'>{item.descripcion}</p>
-                    <p className="categoria">{item.categoria}</p>
-                    <p className="precio">{item.precio}</p>
-                </div>
-            </div>
-        </div>
+        <Card className='container' style={{ maxWidth: '400px', margin: '20px auto', boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', borderRadius: '10px' }}>
+            <Card.Img variant="top" src={item.imagen} alt={item.nombre} style={{ borderRadius: '10px 10px 0 0' }} />
+            <Card.Body>
+                <Card.Title className='titulo' style={{ fontWeight: 'bold' }}>{item.nombre}</Card.Title>
+                <Card.Text className='descripcion'>{item.descripcion}</Card.Text>
+                <Card.Text className="categoria" style={{ fontStyle: 'italic', color: 'gray' }}>{item.categoria}</Card.Text>
+                <Card.Text className="precio" style={{ fontSize: '1.2em', color: 'green', fontWeight: 'bold' }}>${item.precio}</Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
