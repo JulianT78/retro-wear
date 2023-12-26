081e537b5098638208e22fe7b144741227706eae
Render78 <mariojuliantor@hotmail.com>
2023-12-26 18:39:00 -0300

    cambio de imagenes en json y navegacion funcionando correctamente

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Item from './Item'
 
-const ItemList = ({ productos }) => {
+const ItemList = ({ productos, titulo }) => {
     return (
         <div className='container'>
-            <h2 className='titulo'>Productos en venta</h2>
+            <h2 className='titulo'>{titulo}</h2>
             <div className="productos">
                 {productos.map((producto) => <Item producto={producto} key={producto.id} />)}
             </div>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 const ItemListContainer = () => {
 
     const [productos, setProductos] = useState([]);
+    const [titulo, setTitulo] = useState("Productos");
     // console.log(productos);
 
     const categoria = useParams().categoria;
@@ -16,17 +17,18 @@ const ItemListContainer = () => {
             .then((res) => {
                 if (categoria) {
                     setProductos(res.filter((p) => p.categoria === categoria));
+                    setTitulo(categoria);
                 } else {
                     setProductos(res);
+                    setTitulo("Productos");
                 }
-                
             })
-    }, [])
+    }, [categoria])
 
 
     return (
         <div>
-            <ItemList productos={productos} />
+            <ItemList productos={productos} titulo={titulo} />
         </div>
     )
 }
