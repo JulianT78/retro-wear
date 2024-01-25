a71bce7e38c2196aaa380ec0a014ed3181b73cb7
Render78 <mariojuliantor@hotmail.com>
2024-01-25 13:55:09 -0300

    context con localstorage completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,35 +6,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { CartContext } from './context/CartContext'
+import { CartProvider } from './context/CartContext'
 import Carrito from './components/Carrito'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const [carrito, setCarrito] = useState([]);
-
-  const agregarAlCarrito = (item, cantidad) => {
-    const itemAgregado = { ...item, cantidad };
-
-    const nuevoCarrito = [...carrito];
-    const estaElProducto = nuevoCarrito.find(producto => producto.id === itemAgregado.id);
-
-    if (estaElProducto) {
-      estaElProducto.cantidad += cantidad;
-    } else {
-      nuevoCarrito.push(itemAgregado);
-    }
-    setCarrito(nuevoCarrito);
-  }
-
-  const cantidadCarrito = () => {
-    return carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
-  }
-
   return (
     <div>
-      <CartContext.Provider value={{ carrito, agregarAlCarrito, cantidadCarrito }}>
+      <CartProvider>
         <BrowserRouter>
           <NavBar />
 
@@ -48,7 +25,7 @@ function App() {
           </Routes>
 
         </BrowserRouter>
-      </CartContext.Provider>
+      </CartProvider>
     </div>
   )
 }
--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CartContext } from '../context/CartContext'
 
 const Carrito = () => {
+
+  const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
+
+  const handleVaciar = () => {
+    vaciarCarrito();
+  }
+
   return (
-    <div>Carrito</div>
+    <div className='container'>
+      <h1>Carrito</h1>
+
+      {
+        carrito.map((prod) => (
+          <div key={prod.id}>
+            <br />
+            <h3>{prod.nombre}</h3>
+            <p>Precio unitario:{prod.precio}</p>
+            <p>Cantidad: {prod.cantidad}</p>
+            <p>Precio total: ${prod.precio * prod.cantidad}</p>
+          </div>
+        ))
+      }
+
+      {
+        carrito.length > 0 ?
+        <div>
+          <h2>Precio total: ${precioTotal()}</h2>
+          <button onClick={handleVaciar} className='btn btn-danger'>Vaciar</button>
+        </div> :
+        <h2>El carrito está vacío</h2>
+      }
+
+    </div>
   )
 }
 
